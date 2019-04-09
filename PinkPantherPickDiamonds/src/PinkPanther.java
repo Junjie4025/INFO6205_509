3ec760ed9c31f24b46efff4343476f3eb0e2e838
SissiChenxy <chenxysissi@gmail.com>
2019-04-09 16:21:04 -0400

    add functions in pink panther and map class

--- PinkPantherPickDiamonds/src/PinkPanther.java
+++ PinkPantherPickDiamonds/src/PinkPanther.java
@@ -8,4 +8,21 @@ public class PinkPanther {
         score = 0;
         pass = 0;
     }
+
+    public int getPass() {
+        return pass;
+    }
+
+    public void setPass(int pass) {
+        this.pass = pass;
+    }
+
+    public Integer getScore(){
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
 }
