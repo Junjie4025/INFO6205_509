4f4a217aef40ed8c796a34b5aacbd54e0dbf7c58
SissiChenxy <chenxysissi@gmail.com>
2019-04-09 14:44:51 -0400

    create pink panther and map class files

---
+++ PinkPantherPickDiamonds/src/Map.java
@@ -0,0 +1,21 @@
+public class Map {
+    private char[][] map;
+    private final int ROW = 50;
+    private final int COL = 50;
+//    private double diamonds;
+//    private double caps;
+//    private double passes;
+//    private double walls;
+
+    public Map(){
+        this.map = new char[ROW][COL];
+//        this.diamonds = 0.25 * ROW * COL;
+//        caps = 0.2 * ROW * COL;
+//        passes = 0.05 * ROW * COL;
+//        walls = 0.1 * ROW * COL;
+    }
+
+    public void initMap(){
+
+    }
+}
---
+++ PinkPantherPickDiamonds/src/PinkPanther.java
@@ -0,0 +1,11 @@
+public class PinkPanther {
+    private int[] genre;
+    private Integer score;
+    private int pass;
+
+    public PinkPanther(){
+        genre = new int[500];
+        score = 0;
+        pass = 0;
+    }
+}
