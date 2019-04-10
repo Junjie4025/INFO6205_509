a5172d8d7d329963f5a99e486cd584d406c851f9
Junjie <hejunjie3617@gmail.com>
2019-04-09 20:45:37 -0400

    complete evolution process and corresponding unit test

--- PinkPantherPickDiamonds/src/test/PinkPantherTest.java
+++ PinkPantherPickDiamonds/src/test/PinkPantherTest.java
@@ -1,5 +1,42 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class PinkPantherTest {
+
+
+
+    @Test
+    public void selectionTest() {
+
+        List<PinkPanther> list = new ArrayList<>(200);
+        for(int i = 0; i < 200; i++) {
+            list.add(new PinkPanther());
+        }
+
+        List<PinkPanther> testList = EvolutionProcess.selection(list);
+        Assert.assertTrue(testList.size() == 100);
+
+
+
+    }
+
+    @Test
+    public void evolutionTest() {
+
+        List<PinkPanther> list = new ArrayList<>(100);
+        for(int i = 0; i < 100; i++) {
+            list.add(new PinkPanther());
+        }
+
+        List<PinkPanther> testList = EvolutionProcess.evolution(list);
+
+        Assert.assertTrue(testList.size() == 200);
+
+    }
 
 }
