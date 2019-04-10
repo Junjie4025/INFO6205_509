2ba92e46785c844e7ce4e55cf09799bad62f5144
Junjie <hejunjie3617@gmail.com>
2019-04-09 20:49:42 -0400

    update unit test

--- PinkPantherPickDiamonds/src/test/PinkPantherTest.java
+++ PinkPantherPickDiamonds/src/test/PinkPantherTest.java
@@ -10,33 +10,4 @@ public class PinkPantherTest {
 
 
 
-    @Test
-    public void selectionTest() {
-
-        List<PinkPanther> list = new ArrayList<>(200);
-        for(int i = 0; i < 200; i++) {
-            list.add(new PinkPanther());
-        }
-
-        List<PinkPanther> testList = EvolutionProcess.selection(list);
-        Assert.assertTrue(testList.size() == 100);
-
-
-
-    }
-
-    @Test
-    public void evolutionTest() {
-
-        List<PinkPanther> list = new ArrayList<>(100);
-        for(int i = 0; i < 100; i++) {
-            list.add(new PinkPanther());
-        }
-
-        List<PinkPanther> testList = EvolutionProcess.evolution(list);
-
-        Assert.assertTrue(testList.size() == 200);
-
-    }
-
 }
