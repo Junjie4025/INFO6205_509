55aa0eac69166401abb3c5bc3bff3c636e0dac36
SissiChenxy <chenxysissi@gmail.com>
2019-04-10 21:45:56 -0400

    add unit tests

--- PinkPantherPickDiamonds/src/test/MapTest.java
+++ PinkPantherPickDiamonds/src/test/MapTest.java
@@ -1,5 +1,27 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MapTest {
+    @Test
+    public void mapSizeTest(){
+        Map m = new Map();
+        Assert.assertTrue(m.getCOL() == 50);
+        Assert.assertTrue(m.getROW() == 50);
+    }
+
+    @Test
+    public void mapContentTest(){
+        Map m = new Map();
+        m.initMap();
+        char[][] map = m.getMap();
+        for(int i =0; i < m.getROW(); i++){
+            for(int j = 0; j < m.getCOL(); j++){
+                boolean content = map[i][j] == 'D' || map[i][j] == 'C' || map[i][j] == 'P' || map[i][j] == 'W' || map[i][j] == 'B';
+                Assert.assertTrue(content == true);
+            }
+        }
+    }
 
 }
