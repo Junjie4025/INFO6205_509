14ec7b735c16368b61273b164c4ad058655a6144
陈昕昀 <chenxinyun@Sissi-MacBook-Pro.local>
2019-04-19 16:40:22 -0400

    modify unit tests

--- PinkPantherPickDiamonds/src/test/PinkPantherTest.java
+++ PinkPantherPickDiamonds/src/test/PinkPantherTest.java
@@ -4,20 +4,20 @@ import org.junit.Test;
 public class PinkPantherTest {
 
     @Test
-    public void pinkPantherGenreTest() {
+    public void pinkPantherGenreLengthTest() {
 
         PinkPanther p = new PinkPanther();
         int[] genre = p.getGenre();
         Assert.assertTrue(genre.length == 500);
+    }
 
+    @Test
+    public void pinkPantherGenreContentTest() {
+        PinkPanther p = new PinkPanther();
+        int[] genre = p.getGenre();
         for(int i = 0; i < genre.length; i++){
             boolean content = genre[i] == 0 || genre[i] == 1 || genre[i] == 2 || genre[i] == 3;
             Assert.assertTrue(content == true);
         }
     }
-
-
-
-
-
 }
